7d45a44b31b86c57c6729696e7725ef278e1f5c9
cisong <cisong20@gmail.com>
2026-04-11 16:27:08 +0900

    solved ac 접속 에러 time out 발생하여 time out 5->10 으로 변경

--- solved_api.py
+++ solved_api.py
@@ -11,7 +11,7 @@ def get_users(handles):
         "User-Agent": "Mozilla/5.0"
     }
 
-    r = requests.get(url, headers=headers, timeout=5)
+    r = requests.get(url, headers=headers, timeout=10)
 
     if r.status_code != 200:
         print("API ERROR:", r.status_code)
